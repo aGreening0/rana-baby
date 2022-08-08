6c8ae7604143c4ca8b7f7d85e82b6093c3269e63
aGreening0 <103645417+aGreening0@users.noreply.github.com>
2022-08-08 01:31:13 -0500

    Update create.blade.php

--- resources/views/baby/create.blade.php
+++ resources/views/baby/create.blade.php
@@ -3,48 +3,49 @@
 
     </x-slot>
 
-    <div class="py-12">
+    <div class="">
 
-        <div class="max-w-md mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-lg sm:px-6 lg:px-8 m-8">
 
 
-            <div class="card bg-primary text-primary-content">
-                <div class="card-body">
-                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <div class="card bg-base-100 shadow-xl">
+                <div class="card-body float-center">
+                    <h2 class="font-semibold card-title text-xl leading-tight">
                         Welcome! Tell us about your baby...
                     </h2>
                     <form method="POST" action="{{ route('babies.store') }}">
                         @csrf
-                        <div class="flex flex-wrap pt-6 -mx-3 mb-4">
+                        <div class="flex flex-wrap pt-6 mb-4">
                             <div class="w-full">
-                                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                                <label class="block uppercase tracking-wide base-content text-xs font-bold mb-2" for="grid-city">
                                     What is Your Baby's Name?
                                 </label>
-                                <input class="input w-full max-w-xs" id="grid-city" type="text" name="name" placeholder="Yasmin">
+                                <input class="input input-primary w-full max-w-lg" id="grid-city" type="text" name="name" placeholder="Yasmin">
                             </div>
                         </div>
-                        <div class="flex flex-wrap -mx-3 mb-4">
+                        <div class="flex flex-wrap mb-4">
                             <div class="w-full">
-                                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                                <label class="block uppercase tracking-wide base-content text-xs font-bold mb-2" for="grid-city">
                                     What is Your Baby's Birthday?
                                 </label>
-                                <input class="input w-full max-w-xs" id="grid-city" type="text" name="dob" placeholder="mm/dd/yyyy">
+                                <input class="input input-primary w-full max-w-lg" id="grid-city" type="text" name="dob" placeholder="mm/dd/yyyy">
                             </div>
                         </div>
-                        <div class="flex flex-wrap -mx-3 mb-4">
+                        <div class="flex flex-wrap mb-4">
                             <div class="w-full">
-                                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                                <label class="block uppercase tracking-wide base-content text-xs font-bold mb-2" for="grid-city">
                                     What is Your Baby Eating?
                                 </label>
-                                <select class="input w-full max-w-xs" id="grid-city" name="milk" placeholder="">
+                                <select class="input input-primary w-full max-w-lg" id="grid-city" name="milk" placeholder="">
                                     <option value="1">Exclusively Breastfed</option>
                                     <option value="2">Exclusively Formula</option>
                                     <option value="3">Both Breastmilk and Formula</option>
                                 </select>
                             </div>
                         </div>
-
-                        <button class="px-4 py-2 rounded-md text-sm font-medium border-0 focus:outline-none focus:ring transition text-white bg-purple-500 hover:bg-purple-600 active:bg-purple-700 focus:ring-purple-300" type="submit">Button</button>
+                        <div class="float-right mt-10">
+                            <button class="btn btn-primary btn-large btn-responsive" type="submit">Submit</button>
+                        </div>
 
                     </form>
             </div>
